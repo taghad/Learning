5f2a6f0e5ab5309913b2cd20bad935a25cf3cdaf
mohamadHasan <mhtaghad99@gmail.com>
2019-11-09 13:20:08 +0330

    update doubly linkedlist

--- go/DoublyLinkedList/main.go
+++ go/DoublyLinkedList/main.go
@@ -20,10 +20,28 @@ func (e *node) addNewNode(head *node, value int) *node{
 	}
 	next.next = new(node)
 	next.next.value = value
+	next.next.parent = next
 	return head
 }
 
 func (e *node) deleteNode(head *node, value int) *node{
+
+	if head.value == value {
+		if  head.next == nil{
+			return nil
+		}
+		head.value = head.next.value
+		head.next = head.next.next
+		return head
+	}
+	for next := head; next !=nil;  next = next.next {
+		if next.value == value {
+			next.parent.next = next.next
+			next.next.parent = next.parent
+			return head
+		}
+	}
+	return head
 
 }
 
@@ -35,8 +53,27 @@ func (e *node) printList(head *node)  {
 }
 
 func (e *node) printListViceVersa(head *node)  {
+	next := head
+	for next.next != nil{
+		next = next.next
+	}
 
+	for next.parent != nil{
+		fmt.Println(next.value,"<-")
+		next = next.parent
+	}
 }
 func main()  {
+	var head  *node
+	head = head.addNewNode(head,12)
+	head = head.addNewNode(head,11)
+	head = head.addNewNode(head,10)
+	head = head.addNewNode(head,9)
+	head = head.addNewNode(head,8)
+	head = head.addNewNode(head,7)
+	head = head.deleteNode(head,12)
+	head = head.deleteNode(head,9)
+	head = head.deleteNode(head,7)
 
+	head.printListViceVersa(head)
 }
